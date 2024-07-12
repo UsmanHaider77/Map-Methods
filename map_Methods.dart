3b0a8402bcbbdf4af05781276c4d0cdfaac473c4
UsmanHaider77 <musmanhaider77@gmail.com>
2024-07-12 19:15:29 +0500

    Methods of Map

---
+++ map_Methods.dart
@@ -0,0 +1,12 @@
+//********************01[Add()]********************/
+// void main() { 
+//    Map details = {'Usrname':'tom','Password':'pass@123'}; 
+//    details['Uid'] = 'U1oo1'; 
+//    print(details); 
+// } 
+
+//********************01[Keys()]********************/
+void main() { 
+   Map details = {'Usrname':'tom','Password':'pass@123'}; 
+   print(details.keys); 
+}
