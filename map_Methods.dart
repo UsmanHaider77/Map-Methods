b8330d9df04bb244d6247c8fe92b5f7971a2b3ff
UsmanHaider77 <musmanhaider77@gmail.com>
2024-07-13 18:45:05 +0500

    Methods of Map

--- map_Methods.dart
+++ map_Methods.dart
@@ -1,12 +1,24 @@
 //********************01[Add()]********************/
-// void main() { 
-//    Map details = {'Usrname':'tom','Password':'pass@123'}; 
-//    details['Uid'] = 'U1oo1'; 
-//    print(details); 
-// } 
+// void main() {
+//    Map details = {'Usrname':'Haider','Password':'pass@123'};
+//    details['Uid'] = 'U1oo1';
+//    print(details);
+// }
 
-//********************01[Keys()]********************/
-void main() { 
-   Map details = {'Usrname':'tom','Password':'pass@123'}; 
-   print(details.keys); 
+//********************02[Keys()]********************/
+// void main() {
+//    Map details = {'Usrname':'Haider','Password':'pass@123'};
+//    print(details.keys);
+// }
+
+//********************03[isEmpty()]********************/
+// void main() {
+//   Map details = {'Usrname': 'Haider', 'Password': 'pass@123'};
+//   print(details.isEmpty);
+// }
+
+//********************04[isNotEmpty()]********************/
+void main() {
+  Map details = {'Usrname': 'Haider', 'Password': 'pass@123'};
+  print(details.isNotEmpty);
 }
