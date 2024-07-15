3c3fb859b34e151746082453f07e4d3f830c75b7
UsmanHaider77 <musmanhaider77@gmail.com>
2024-07-15 10:00:33 +0500

    Methods of Map

--- map_Methods.dart
+++ map_Methods.dart
@@ -18,7 +18,60 @@
 // }
 
 //********************04[isNotEmpty()]********************/
+// void main() {
+//   Map details = {'Usrname': 'Haider', 'Password': 'pass@123'};
+//   print(details.isNotEmpty);
+// }
+
+//********************05[Values()]********************/
+// void main() {
+//   Map details = {'Usrname': 'Haider', 'Password': 'pass@123'};
+//   print(details.values);
+// }
+
+//********************06[ContainKey()]********************/
+// void main() {
+//   Map details = {'Usrname': 'Haider', 'Password': 'pass@123'};
+//   print(details.containsKey('Usrname'));
+// }
+
+//********************07[ContainValue()]********************/
+// void main() {
+//   Map details = {'Usrname': 'Haider', 'Password': 'pass@123'};
+//   print(details.containsValue("Haider"));
+// }
+
+//********************08[PutIfAbsent()]********************/
+// void main() {
+//   Map details = {'Usrname': 'Haider', 'Password': 'pass@123'};
+//   details.putIfAbsent("Class", () => "Flutter");
+//   print(details);
+// }
+
+//********************09[AddAll()]********************/
+// void main() {
+//   Map details = {'Usrname': 'Haider', 'Password': 'pass@123'};
+//   details.addAll({"Class":"Flutter","Section":"A"});
+//   print(details);
+// }
+
+//********************10[RemoveWhere()]********************/
+// void main() {
+//   Map details = {'Usrname': 'Haider', 'Password': 'pass@123','Section':'A'};
+//   details.removeWhere((key, value) => key=='Section' && value=='A');
+//   print(details);
+// }
+
+//********************11[Remove()]********************/
+// void main() {
+//   Map details = {'Usrname': 'Haider', 'Password': 'pass@123','Section':'A'};
+//   details.remove('Usrname');
+//   print(details);
+// }
+
+//********************12[UpdateAll()]********************/
 void main() {
-  Map details = {'Usrname': 'Haider', 'Password': 'pass@123'};
-  print(details.isNotEmpty);
+  Map details = {'Usrname': 'Haider', 'Password': 'pass@123','Section':'A'};
+  details.updateAll((key, value) => value.toUpperCase());
+  print(details);
 }
